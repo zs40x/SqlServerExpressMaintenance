bec15dff02c0b0e3cb206666bc512b6fc8adb471
Stefan Mehnert <StefanMehnert@gmail.com>
2019-01-08 10:47:32 +0100

    Update Daily_Full_Backup.sql

--- Daily_Full_Backup.sql
+++ Daily_Full_Backup.sql
@@ -1,5 +1,17 @@
 exec master.dbo.expressmaint
-   @database      = 'DbName',
+   @database      = 'ALL_USER',
+   @optype        = 'LOG',
+   @backupfldr    = 'C:\SqlBackup\TLogBackup\',
+   @reportfldr    = 'C:\SqlBackup\Reports\',
+   @verify        = 1,
+   @dbretainunit  = 'days',
+   @dbretainval   = 2,
+   @rptretainunit = 'weeks',
+   @rptretainval  = 1,
+   @report        = 1;
+
+exec master.dbo.expressmaint
+   @database      = 'ALL_USER',
    @optype        = 'DB',
    @backupfldr    = 'C:\SqlBackup\Backups\',
    @reportfldr    = 'C:\SqlBackup\Reports\',
@@ -8,4 +20,42 @@ exec master.dbo.expressmaint
    @dbretainval   = 2,
    @rptretainunit = 'weeks',
    @rptretainval  = 1,
-   @report        = 1
+   @report        = 1;
+
+exec master.dbo.expressmaint
+   @database      = 'ALL_USER',
+   @optype        = 'CHECKDB',
+   @backupfldr    = 'C:\SqlBackup\Backups\',
+   @reportfldr    = 'C:\SqlBackup\Reports\',
+   @verify        = 1,
+   @dbretainunit  = 'days',
+   @dbretainval   = 2,
+   @rptretainunit = 'weeks',
+   @rptretainval  = 1,
+   @report        = 1;
+
+
+exec master.dbo.expressmaint
+   @database      = 'ALL_USER',
+   @optype        = 'REINDEX',
+   @backupfldr    = 'C:\SqlBackup\Backups\',
+   @reportfldr    = 'C:\SqlBackup\Reports\',
+   @verify        = 1,
+   @dbretainunit  = 'days',
+   @dbretainval   = 2,
+   @rptretainunit = 'weeks',
+   @rptretainval  = 1,
+   @report        = 1;
+
+
+exec master.dbo.expressmaint
+   @database      = 'ALL_USER',
+   @optype        = 'REORG',
+   @backupfldr    = 'C:\SqlBackup\Backups\',
+   @reportfldr    = 'C:\SqlBackup\Reports\',
+   @verify        = 1,
+   @dbretainunit  = 'days',
+   @dbretainval   = 2,
+   @rptretainunit = 'weeks',
+   @rptretainval  = 1,
+   @report        = 1;
